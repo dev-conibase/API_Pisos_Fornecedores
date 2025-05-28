c72592812aca3a0332996547dcdded365fbe7c93
dev-conibase <tn@conibase.com.br>
2025-05-28 10:18:50 -0300

    API atualizada 28/05/2025. Formigres não atualizou.

--- pages/api/incopisos.js
+++ pages/api/incopisos.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customLastUpdated = "27/05/2025";
+const customLastUpdated = "28/05/2025";
 
 export default function handler(req, res) {
   const { produto } = req.query;
