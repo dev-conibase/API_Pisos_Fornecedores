9a035d01dfec60ac3975c6cbf947d4cdbfdcd026
dev-conibase <tn@conibase.com.br>
2025-06-10 13:46:26 -0300

    API atualizada 10/06/2025.

--- pages/api/helena.js
+++ pages/api/helena.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customLastUpdated = "09/06/2025";
+const customLastUpdated = "10/06/2025";
 
 export default function handler(req, res) {
   const { produto } = req.query;
