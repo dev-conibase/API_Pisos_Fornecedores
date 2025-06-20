73f22895339a1bf828cef999a3b6578aba210da2
dev-conibase <tn@conibase.com.br>
2025-06-20 14:31:09 -0300

    API atualizada 20/06/2025.

--- pages/api/incopisos.js
+++ pages/api/incopisos.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customLastUpdated = "18/06/2025";
+const customLastUpdated = "20/06/2025";
 
 export default function handler(req, res) {
   const { produto } = req.query;
