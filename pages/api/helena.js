9695278ec8d0f6e3b4a05d4c876ae58607f90642
dev-conibase <tn@conibase.com.br>
2025-07-01 10:01:51 -0300

    API atualizada 01/07/2025. Formigres não atualizou.

--- pages/api/helena.js
+++ pages/api/helena.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customLastUpdated = "30/06/2025";
+const customLastUpdated = "01/07/2025";
 
 export default function handler(req, res) {
   const { produto } = req.query;
