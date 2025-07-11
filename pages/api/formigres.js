4985752a83c219950b4bb3c9fcae9286e88b6615
dev-conibase <tn@conibase.com.br>
2025-07-11 10:59:15 -0300

    API atualizada 11/07/2025.

--- pages/api/formigres.js
+++ pages/api/formigres.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Defina a data customizada
-const customLastUpdated = "10/07/2025";
+const customLastUpdated = "11/07/2025";
 
 export default function handler(req, res) {
   const { produto } = req.query;
