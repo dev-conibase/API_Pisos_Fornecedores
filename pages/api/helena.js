dcc9a9ff35e9cb3b06c11237c777c0b524c6ede2
dev-conibase <tn@conibase.com.br>
2025-12-09 07:04:33 -0300

    Atualizacao automatica dos JSONs e datas customLastUpdated

--- pages/api/helena.js
+++ pages/api/helena.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customLastUpdated = "06/12/2025";
+const customLastUpdated = "08/12/2025";
 
 export default function handler(req, res) {
   const { produto } = req.query;
