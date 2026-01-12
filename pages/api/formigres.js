06b57065806330b3916abb64b7fdb2c6a3460763
dev-conibase <tn@conibase.com.br>
2026-01-12 17:28:02 -0300

    Atualizacao automatica dos JSONs e datas customLastUpdated

--- pages/api/formigres.js
+++ pages/api/formigres.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Data da última atualização
-const customLastUpdated = "12/01/2025";
+const customLastUpdated = "12/01/2026";
 
 export default function handler(req, res) {
   const { produto } = req.query;
