38a0039789f75df468f78010b07f86a32515e50a
dev-conibase <tn@conibase.com.br>
2026-02-13 15:39:30 -0300

    Atualizacao automatica dos JSONs e datas customLastUpdated

--- pages/api/incopisos.js
+++ pages/api/incopisos.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customLastUpdated = "12/02/2026";
+const customLastUpdated = "13/02/2026";
 
 export default function handler(req, res) {
   const { produto } = req.query;
